2a0dce7f5cd9117b6d4fd1b9cf1369a2b77b6f24
Michael Stapelberg <michael@stapelberg.de>
2025-02-02 09:44:58 +0100

    regenerate: print good error for missing node_modules directory
    related to https://github.com/gokrazy/bull/issues/7

--- regenerate.sh
+++ regenerate.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+if [ ! -d "third_party/codemirror/node_modules" ]
+then
+    echo "Cannot re-generate CodeMirror JavaScript bundle:" >&2
+    echo "" >&2
+    echo "	No node_modules directory found in third_party/codemirror!" >&2
+    echo "" >&2
+    echo "To install the CodeMirror editor and dependencies into third_party," >&2
+    echo "install the npm JavaScript package manager and run:" >&2
+    echo "" >&2
+    echo "	(cd third_party/codemirror && npm install)" >&2
+    echo "" >&2
+    exit 2
+fi
+
 export NODE_PATH=$PWD/third_party/codemirror/node_modules
 go tool esbuild \
    internal/codemirror/bull-codemirror.ts \
